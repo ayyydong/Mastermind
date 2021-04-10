d28f1a527cff012fe76518aa2a9a73f80880e2bf
dongandy <andydong0928@gmail.com>
2021-04-10 13:56:49 -0700

    basic implementation

--- main.pl
+++ main.pl
@@ -1,6 +1,36 @@
 % main entry point of the game
 
-guess(A,B,C,D):-countcolor([A,B,C,D],0),nl,countcorrect([A,B,C,D],0,1).
+:- module(board, [initialize/2]).
+:- use_module(library(random)).
+
+gameBoard(
+	['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_'],
+    ['_', '_', '_', '_']).
+
+% Initialize empty board
+initialize(gameBoard(X)).
+
+displayBoard(gameBoard(X)):- write (' ? ? ? ?'), nl.
+
+mastermind:- initialize(X),
+	   displayBoard(X),
+	   nextMove('X',X), !.
+
+guess(A,B,C,D):- countcolor([A,B,C,D],0),nl,countcorrect([A,B,C,D],0,1).
+
+% updateBoard().
+
+nextMove(X):- triesleft(N).
+		  write('Machine wins!').
+nextMove(X):- guess(A,B,C,D).
+		  write('You win!').
 
 countcolor([],Count1):-
 write(Count1),
@@ -24,6 +54,8 @@ countcorrect([H|T],Count1,Pos1):-
 countcorrect([_|T],Count1,Pos1):-
     Pos is Pos1 + 1,
     countcorrect(T,Count1,Pos).
+
+triesleft(N):- N == 9.
 
 ans(1,1).
 ans(2,2).
