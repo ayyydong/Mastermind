45b7a4c906112b6168493dcb518da68e3c93be2e
dongandy <andydong0928@gmail.com>
2021-04-16 05:59:35 -0700

    new changes

--- board.pl
+++ board.pl
@@ -1,26 +1,30 @@
 % the gameboard 
 :- use_module(library(random)).
 
-% store value of game solution as key(List).  Will randomize for each new instance of swipl.
-:- table key/1. 
-key(List) :- randseq(4,6,List).
-
 % Initialize empty board
-initialize(gameBoard([
+initialize(10, gameBoard([
 	['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
     ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
     ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
     ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']])).
 
+initialize(8, gameBoard([
+	['_', '_', '_', '_', '_', '_', '_', '_'],
+    ['_', '_', '_', '_', '_', '_', '_', '_'],
+    ['_', '_', '_', '_', '_', '_', '_', '_'],
+    ['_', '_', '_', '_', '_', '_', '_', '_']])).
 
 displayBoard(gameBoard(X), 0, List):- 
+	writeln("end"),
 	atomic_list_concat(List, ' ', FinalAtom),
 	atom_concat('   ',FinalAtom,A),
 	write(A), 
 	nl,
 	show(X,10).
 displayBoard(gameBoard(X), N, List):- 
-	write('   ? ? ? ?'), 
+	atomic_list_concat(List, ' ', FinalAtom),
+	atom_concat('   ',FinalAtom,A),
+	write(A), 
 	nl,
 	show(X,10).
 
@@ -42,15 +46,15 @@ showLineHelper([],_).
 showLineHelper([[X|X2]|XS],[X2|XS2]):- write(X), write(' '),
 			          showLineHelper(XS,XS2).
 
-uncoverAnswer(X,[H|T]):-
-	displayBoard(X, 0).
+uncoverAnswer(X,[H|T], List):-
+	displayBoard(X, 0, List).
 
 mastermind:- 
-	makeGreeting, 
-	initialize(X),
-	displayBoard(X,10,List), !.
+	makeGreeting(2,1),
+	!.
      
-makeGreeting :- 
+makeGreeting(D, M) :- 
+	repeat,
     writeln('\e[H\e[2J'),
     writeln("			   █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀								"),
     writeln("			   ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄								"),
@@ -65,25 +69,145 @@ makeGreeting :-
     writeln("██║░╚═╝░██║██║░░██║██████╔╝░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║██║░╚███║██████╔╝"),
     writeln("╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═════╝░"),
     nl, 
-    writeln("Instructions [ Enter 1 ]"),
+    writeln("How to play/ Information about the game [ Enter 1 ]"),
     nl,
     writeln("Settings [ Enter 2 ]"),
     nl,
     writeln("Load Game [ Enter 3 ]"),
     nl,
-    readChoice(C).
+    readChoice(C, D, M, Hints, Guesses, AIs).
 
-readChoice(C) :-
+readChoice(C, D, M, Hints, Guesses, AIs) :-
+	writeln(D),
+	writeln(M),
 	get_char(C),
-	C == '1',
-	writeln('\e[H\e[2J'),
-	nl,
-	writeln('Guess the 4-digit key by typing: guess(N1,N2,N3,N4) where each N is a unique number from 1 to 6.'), 
-	writeln('Duplicates are not allowed. The computer will return clues based on your guesses.'),
-	writeln('You will have 10 guesses before the answer is revealed and the bot wins...'), 
-    nl, !.
+	(C == '1' -> 
+		writeln('\e[H\e[2J'),
+		writeln("User single player: "),
+		nl,
+		writeln('Guess the 4-digit key by typing: guess(N1,N2,N3,N4) where each N is a unique number from 1 to 6.'), 
+		writeln('Duplicates are not allowed. The bot will return clues based on your guesses.'),
+		writeln('You will have 10 guesses before the answer is revealed and the bot wins...'),
+		nl,
+		nl,
+		nl,
+		writeln("User vs Bot: "),
+		nl,
+		writeln("A number generator will randomly generate a 4-digit key. The user and the bot will compete to guess the 4-digit key first."),
+		writeln('Each player will take turns guessing. A draw will be when the board is full'),
+		nl,
+    	nl,
+		writeln('Enter anything to return to home page.'),
+		get_char(K),
+		(K == '1' ->
+			fail
+		; fail
+		)
+	; C == '2' ->
+		repeat,
+		writeln('\e[H\e[2J'),
+		% writeln(D), writeln(M), 
+		nl,
+		writeln("Choose difficulty level (Default: Medium) [ Enter 1 ]"),
+		nl,
+		writeln("Choose gamemode (Default: User single player) [ Enter 2 ]"),
+		nl,
+    	nl,
+		writeln('Enter anything else to return to home page.'),
+	    readInnerChoice(K, D, M, Hints, Guesses, AIs)
+	; C == '3' -> 
+		writeln('\e[2J'),
+		initialize(Guesses, X), 
+		randseq(4,6,List),
+		displayBoard(X,Guesses,List),
+		!
+	; 	writeln("Not a valid input!"),
+		sleep(0.2),
+		fail
+	).
 
-% nextMove(X, 10), !.
+readInnerChoice(K, D, M, Hints, Guesses, AIs) :-
+	get_char(K),
+	(K == '1' ->
+		writeln('\e[H\e[2J'),
+		writeln(D), writeln(M), 
+		writeln("Difficulties: "),
+		nl,
+		nl,
+		writeln("Easy [ Enter 1 ]"),
+		nl,
+		writeln("Medium [ Enter 2 ]"),
+		nl,
+		writeln("Hard [ Enter 3 ]"),
+		nl,
+		nl,
+		writeln('Enter anything else to go back.'),
+		get_char(S),
+		(S == '1' -> 
+			(M == '1' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+			; M == '2' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+			; writeln("c")
+			)
+		; S == '2' ->
+		 	(M == '1' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+		 	; M == '2' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+		 	; writeln("b")
+		 	)
+		; S == '3' -> 
+			(M == '1' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+			; M == '2' -> chosenSetting(S, M, Hints, Guesses, AIs), fail
+			; writeln("a")
+			)
+		; writeln("A")
+		)
+	; K == '2' ->
+		writeln('\e[H\e[2J'),
+		writeln(D), writeln(M), 
+		writeln("Gamemodes: "),
+		nl,
+		nl,
+		writeln("User single player [ Enter 1 ]"),
+		nl,
+		writeln("User vs Bot [ Enter 2 ]"),
+		nl,
+		nl,
+		writeln('Enter anything else to go back.'),
+		get_char(M),
+		(M == '1' ->
+			(D == '1' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; D == '2' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; D == '3' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; fail
+			)
+		; M == '2' ->
+			(D == '1' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; D == '2' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; D == '3' -> chosenSetting(D, M, Hints, Guesses, AIs), fail
+			; fail
+			)
+		; fail
+		)
+	; makeGreeting(D,M)
+	).
+
+% chosenSetting(D, M, Hints, Guesses, AIs).
+% user gets hints, 10 guesses
+chosenSetting(1, 1, 2, 10, NAI).
+
+% user gets hints, 10 guesses
+chosenSetting(1, 2, 2, 10, NAI).
+
+% no hints, 10 guesses
+chosenSetting(2, 1, 0, 10, NAI).
+
+% no hints, no AI, 10 guesses
+chosenSetting(2, 2, 0, 10, NAI).
+
+% no hints, 8 guesses
+chosenSetting(3, 1, 0, 8, NAI).
+
+% no hints, with AI, 10 guesses total (5 each)
+chosenSetting(3, 2, 0, 10, AI).
 
 guess(A,B,C,D):- countcolor([A,B,C,D],0),nl,countcorrect([A,B,C,D],0,1).
 
